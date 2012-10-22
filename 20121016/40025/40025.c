977004fc8c1320c17c645f3d50b45f844fcd6d86
richard <richard9372@gmail.com>
2012-10-22 14:01:09 +0800

    finish 40025

--- 20121016/40025/40025.c
+++ 20121016/40025/40025.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <math.h>
+#include <memory.h>
+
+int a[100];
+int step;
+
+int cmp(const void *a, const void *b)
+{
+	return *(int*)a - *(int*)b;
+}
 int main(void)
 {
     int factor, m, n, number, sum;
@@ -8,7 +18,33 @@ int main(void)
     for(ri = 1; ri <= repeat; ri++){
         scanf("%d%d", &m, &n);
         printf("result:\n"); 
-		/*---------*/
+		for (number = m; number <= n; ++number)
+		{
+			int i ;
+			memset(a, 0, sizeof(a));
+			step = 0;
+			sum = 1;
+			for (i = 2; i <= sqrt(number); ++i)
+			{
+				if (number % i == 0)
+				{
+					a[step] = i;
+					a[step + 1] = number / i;
+					step += 2;
+					sum += i + number / i;
+				}
+			}
+			if (sum == number)
+			{
+				qsort(a, step, sizeof(int), cmp);
+				printf("%d = 1", number);
+				for (i = 0; i != step; ++i)
+					printf(" + %d", a[i]);
+				printf("\n");
+			}
+				
+
+		}
 		
     }
-}
+} 
