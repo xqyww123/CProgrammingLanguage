82b29a113ddc33676cb1b97d72cf75c998401877
richard <richard9372@gmail.com>
2012-11-07 14:02:32 +0800

    08

--- 60009/60009.c
+++ 60009/60009.c
@@ -4,12 +4,24 @@ int main(void)
     char c;
     int count, word;
     int repeat, ri;
-
+	
     scanf("%d", &repeat);
     getchar();
     for(ri = 1; ri <= repeat; ri++){
         c = getchar();
-/*---------*/
+        count = 0;
+        word = ' ';
+		/*---------*/
+		while (c != '\n')
+		{
+			if (word != ' ' && c == ' ')
+				++count;
+			word = c;
+			c = getchar();
+		}
+		if (word != ' ')
+			++count;
         printf("count = %d\n", count);
     }
+    return 0;
 }
